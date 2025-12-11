7da9e20622428b3b82b6c51f7ffd286479f1023b
eyad22anas-pixel <eyad22anas@gmail.com>
2025-12-10 20:53:36 -0500

    Refactor data loading and filtering in train_model2.py
    Updated data loading process to handle corrupted data and limited the dataset to specific drowsiness labels. Adjusted comments for clarity.

--- train_model2.py
+++ train_model2.py
@@ -3,37 +3,41 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import tensorflow as tf
+#deleate corupterd data
+df = pd.read_csv("datafoeAI1.csv", on_bad_lines='skip', engine='python')
 
-# 1. LOAD CSV DATA
-df = pd.read_csv("datafoeAI1.csv")
+# laod data
+#limit the data to what we need 
+drowsiness_labels = ['Normal', 'Tired', 'Very Tired']
+df = df[df['Label'].isin(drowsiness_labels)]
 
-# Drop rows with missing values in the relevant columns (features + label)
+# Drop rows with missing values
 df = df.dropna(subset=[
     'Label', 'Blink_Rate', 'Avg_Blink_Duration', 'EAR_Mean', 'EAR_Variance', 'Longest_Eye_Closure'
 ])
 
-# 2. SELECT FEATURES AND LABELS
+# labels
 features = df[[
     "Blink_Rate", "Avg_Blink_Duration", "EAR_Mean", "EAR_Variance", "Longest_Eye_Closure"
 ]].values
 
 labels = df["Label"].values
 
-# 3. ENCODE LABELS
+# ENCODE LABELS (so ai understand)
 label_encoder = LabelEncoder()
 y_encoded = label_encoder.fit_transform(labels)
 
-# 4. TRAIN/TEST SPLIT
+# split the data into 8-2 ratio to train and test
 X_train, X_test, y_train, y_test = train_test_split(
     features, y_encoded, test_size=0.2, random_state=42
 )
 
-# 5. NORMALIZE INPUTS
+# normalizing input this is needed for some reason
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# 6. BUILD MODEL
+# make acteull model
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(32, activation='relu', input_shape=(X_train_scaled.shape[1],)),
     tf.keras.layers.Dense(32, activation='relu'),
@@ -48,7 +52,7 @@ model.compile(
 
 model.summary()
 
-# 7. TRAIN MODEL
+# train the thing
 history = model.fit(
     X_train_scaled, y_train,
     epochs=30,
@@ -57,14 +61,14 @@ history = model.fit(
     verbose=1
 )
 
-# 8. EVALUATE MODEL
+# test accuracy
 loss, accuracy = model.evaluate(X_test_scaled, y_test)
 print(f"\nTest Accuracy: {accuracy * 100:.2f}%")
 
-# 9. SAVE MODEL, LABELS, SCALER
+# 9. SAVE MODEL and the stuff that it does
 model.save("blink_ear_aggregate_model.keras")
 np.save("blink_ear_label_classes.npy", label_encoder.classes_)
 np.save("blink_ear_scaler_mean.npy", scaler.mean_)
 np.save("blink_ear_scaler_scale.npy", scaler.scale_)
 
-#Test Accuracy: 100% (sheeeeeeeeeesh crazy)
+#Test Accuracy: 81.82%% (sheeeeeeeeeesh crazy)
